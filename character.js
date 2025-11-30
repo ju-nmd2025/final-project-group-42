5e2c6a89fbed774ff3fe385b744f068cdab481a8
filez34 <sophiakotova@gmail.com>
2025-11-30 20:45:21 +0100

    Update character.js
    cloned this from existing repository on my computer cause i forgot that you need to do assignment here

--- character.js
+++ character.js
@@ -1,22 +1,25 @@
 export default class Character {
-
-    	constructor(x, y, w, h) {
+    
+    constructor(x, y, w, h) {
 		this.x = x;
 		this.y = y;
 		this.w = w;
 		this.h = h;
-//		this.isOnPlatForm = false;
 	}
 
+    
     draw() {
-        rect(this.x, this.y, this.w, this.h);
-    }
+            rect(this.x, this.y, this.w, this.h);
+        }
 
-    isColliding(character, platform) {
-    if (platform.y === character.y + character.w && platform.x <= character.x + character.w) {
-        return true;
-    } else {
+    isColliding(platform) {
+        if ( ((this.y + this.h) >= platform.y) &&
+             ((this.x + this.w) >= platform.x) &&
+             (this.x <= (platform.x + platform.w)) )
+        {
+            return true;
+        }
+
         return false;
     }
-}
 }
